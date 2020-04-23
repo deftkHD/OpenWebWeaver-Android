7a47ff0d07e841a9eb71572b0f0e70a32b4ecbb5
finn <32391033+deftkHD@users.noreply.github.com>
2020-04-23 02:31:22 +0200

    replace UnitUtil.kt with android tools

--- app/src/main/java/de/deftk/lonet/mobile/adapter/FileStorageAdapter.kt
+++ app/src/main/java/de/deftk/lonet/mobile/adapter/FileStorageAdapter.kt
@@ -1,17 +1,15 @@
 package de.deftk.lonet.mobile.adapter
 
 import android.content.Context
+import android.text.format.Formatter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import de.deftk.lonet.api.model.feature.Quota
 import de.deftk.lonet.api.model.feature.files.OnlineFile
-import de.deftk.lonet.mobile.AuthStore
 import de.deftk.lonet.mobile.R
-import de.deftk.lonet.mobile.utils.UnitUtil
 import java.text.DateFormat
 
 class FileStorageAdapter(context: Context, elements: List<OnlineFile>): ArrayAdapter<OnlineFile>(context, 0, elements) {
@@ -22,13 +20,13 @@ class FileStorageAdapter(context: Context, elements: List<OnlineFile>): ArrayAda
 
         listItemView.findViewById<TextView>(R.id.file_name).text = item.name
         if (item.id != "/") {
-            listItemView.findViewById<TextView>(R.id.file_size).text = if(item.type == OnlineFile.FileType.FILE) UnitUtil.getFormattedSize(item.size) else context.getString(R.string.directory)
+            listItemView.findViewById<TextView>(R.id.file_size).text = if(item.type == OnlineFile.FileType.FILE) Formatter.formatFileSize(context, item.size) else context.getString(R.string.directory)
         } else {
             //TODO implement
             //val member = AuthStore.getMember(item.parentId)
             //val quota = member.getFileStorageState(AuthStore.appUser).second
             //listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), UnitUtil.getFormattedSize(quota.free), UnitUtil.getFormattedSize(quota.limit))
-            listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), UnitUtil.getFormattedSize(0), UnitUtil.getFormattedSize(0))
+            listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), "0B", "0B")
         }
         if (item.id != "/")
             listItemView.findViewById<TextView>(R.id.file_modified_date).text = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT).format(item.modificationDate)
--- app/src/main/java/de/deftk/lonet/mobile/adapter/OverviewAdapter.kt
+++ app/src/main/java/de/deftk/lonet/mobile/adapter/OverviewAdapter.kt
@@ -1,17 +1,10 @@
 package de.deftk.lonet.mobile.adapter
 
 import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
-import de.deftk.lonet.mobile.R
 import de.deftk.lonet.mobile.feature.overview.AbstractOverviewElement
-import de.deftk.lonet.mobile.feature.overview.FileStorageOverview
-import de.deftk.lonet.mobile.feature.overview.MailOverview
-import de.deftk.lonet.mobile.feature.overview.SystemNotificationsOverview
-import de.deftk.lonet.mobile.utils.UnitUtil
 
 class OverviewAdapter(context: Context, elements: List<AbstractOverviewElement>): ArrayAdapter<AbstractOverviewElement>(context, 0, elements) {
 
