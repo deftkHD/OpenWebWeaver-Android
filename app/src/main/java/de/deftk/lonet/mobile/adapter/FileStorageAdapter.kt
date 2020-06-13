1f4f9656ed632cef0cbefdee3583a930ea6a80a7
finn <32391033+deftkHD@users.noreply.github.com>
2020-06-13 20:28:26 +0200

    better implementation of file storage

--- app/src/main/java/de/deftk/lonet/mobile/adapter/FileStorageAdapter.kt
+++ app/src/main/java/de/deftk/lonet/mobile/adapter/FileStorageAdapter.kt
@@ -8,34 +8,22 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import de.deftk.lonet.api.model.feature.files.OnlineFile
+import de.deftk.lonet.api.model.Group
+import de.deftk.lonet.api.model.feature.Quota
 import de.deftk.lonet.mobile.R
-import java.text.DateFormat
 
-class FileStorageAdapter(context: Context, elements: List<OnlineFile>): ArrayAdapter<OnlineFile>(context, 0, elements) {
+class FileStorageAdapter(context: Context, val elements: Map<Group, Quota>) : ArrayAdapter<Group>(context, 0, elements.keys.toList()) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val listItemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_file, parent, false)
         val item = getItem(position) ?: return listItemView
 
-        listItemView.findViewById<TextView>(R.id.file_name).text = item.name
-        if (item.id != "/") {
-            listItemView.findViewById<TextView>(R.id.file_size).text = if(item.type == OnlineFile.FileType.FILE) Formatter.formatFileSize(context, item.size) else context.getString(R.string.directory)
-        } else {
-            //TODO reimplement, root filestorages should have own adapter
-            //val member = AuthStore.getMember(item.parentId)
-            //val quota = member.getFileStorageState(AuthStore.appUser).second
-            //listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), UnitUtil.getFormattedSize(quota.free), UnitUtil.getFormattedSize(quota.limit))
-            listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), "0B", "0B")
-        }
-        if (item.id != "/")
-            listItemView.findViewById<TextView>(R.id.file_modified_date).text = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT).format(item.modificationDate)
+        listItemView.findViewById<TextView>(R.id.file_name).text = item.getName()
+        val quota = elements[item] ?: Quota(-1, -1, -1, -1, -1, -1)
+        listItemView.findViewById<TextView>(R.id.file_size).text = String.format(context.getString(R.string.quota), Formatter.formatFileSize(context, quota.free), Formatter.formatFileSize(context, quota.limit))
         val imageView = listItemView.findViewById<ImageView>(R.id.file_image)
-        when (item.type) {
-            OnlineFile.FileType.FILE -> imageView.setImageResource(R.drawable.ic_file)
-            OnlineFile.FileType.FOLDER -> imageView.setImageResource(R.drawable.ic_folder)
-            else -> imageView.setImageDrawable(null)
-        }
+        imageView.setImageResource(R.drawable.ic_folder)
+
         return listItemView
     }
 
--- app/src/main/java/de/deftk/lonet/mobile/fragments/FileStorageFragment.kt
+++ app/src/main/java/de/deftk/lonet/mobile/fragments/FileStorageFragment.kt
@@ -15,14 +15,17 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import de.deftk.lonet.api.model.Group
+import de.deftk.lonet.api.model.feature.Quota
 import de.deftk.lonet.api.model.feature.abstract.IFilePrimitive
 import de.deftk.lonet.api.model.feature.files.OnlineFile
+import de.deftk.lonet.mobile.AuthStore
 import de.deftk.lonet.mobile.R
 import de.deftk.lonet.mobile.abstract.FeatureFragment
 import de.deftk.lonet.mobile.abstract.IBackHandler
 import de.deftk.lonet.mobile.adapter.FileStorageAdapter
+import de.deftk.lonet.mobile.adapter.FileStorageFilesAdapter
 import de.deftk.lonet.mobile.feature.AppFeature
-import de.deftk.lonet.mobile.feature.RootFileProvider
 import de.deftk.lonet.mobile.utils.FileUtil
 import kotlinx.android.synthetic.main.fragment_file_storage.*
 import java.io.File
@@ -32,10 +35,12 @@ import java.util.*
 
 class FileStorageFragment: FeatureFragment(AppFeature.FEATURE_FILE_STORAGE), IBackHandler {
 
-    private val history = Stack<IFilePrimitive>()
+    private val history = Stack<Any?>()
+    private var currentGroup: Group? = null
 
     companion object {
         private const val SAVE_HISTORY = "de.deftk.lonet.mobile.files.history"
+        private const val SAVE_GROUP = "de.deftk.lonet.mobile.files.group"
         const val ARGUMENT_GROUP = "de.deftk.lonet.mobile.files.argument_group"
         const val ARGUMENT_FILE_ID = "de.deftk.lonet.mobile.files.argument_file_id"
     }
@@ -48,11 +53,10 @@ class FileStorageFragment: FeatureFragment(AppFeature.FEATURE_FILE_STORAGE), IBa
         }
 
         if (savedInstanceState != null) {
-            val history = savedInstanceState.getSerializable(SAVE_HISTORY) as Stack<*>
-            history.forEach {
-                this.history.push(it as IFilePrimitive)
+            (savedInstanceState.getSerializable(SAVE_HISTORY) as? Stack<*>)?.forEach {
+                this.history.push(it)
             }
-            println()
+            currentGroup = savedInstanceState.getSerializable(SAVE_GROUP) as? Group
         }
 
         val view = inflater.inflate(R.layout.fragment_file_storage, container, false)
@@ -60,48 +64,73 @@ class FileStorageFragment: FeatureFragment(AppFeature.FEATURE_FILE_STORAGE), IBa
         val swipeRefresh = view.findViewById<SwipeRefreshLayout>(R.id.file_storage_swipe_refresh)
         swipeRefresh.setOnRefreshListener {
             list.adapter = null
-            DirectoryLoadingTask().execute(history.peek())
+            FilesLoadingTask().execute(history.peek())
         }
         list.setOnItemClickListener { _, _, position, _ ->
-            val item = list.getItemAtPosition(position) as OnlineFile
-            if (item.type == OnlineFile.FileType.FOLDER) {
-                navigate(item)
-            } else {
-                FileDownloadOpenTask().execute(item)
-                Toast.makeText(context, getString(R.string.download_started), Toast.LENGTH_SHORT).show()
+            when (val item = list.getItemAtPosition(position)) {
+                is OnlineFile -> {
+                    if (item.type == OnlineFile.FileType.FOLDER) {
+                        navigate(item)
+                    } else {
+                        FileDownloadOpenTask().execute(item)
+                        Toast.makeText(context, getString(R.string.download_started), Toast.LENGTH_SHORT).show()
+                    }
+                }
+                is Group -> {
+                    navigate(item)
+                }
             }
+
         }
         list.setOnItemLongClickListener { _, _, position, _ ->
             val item = list.getItemAtPosition(position)
             //TODO show context menu
             true
         }
-        if (history.isEmpty())
-            history.push(RootFileProvider())
-        navigate(history.pop())
+        navigate(if (history.isNotEmpty()) history.pop() else currentGroup)
         return view
     }
 
-    private fun navigate(directory: IFilePrimitive) {
+    private fun navigate(directory: Any?) {
         file_list?.adapter = null
         progress_file_storage?.visibility = ProgressBar.VISIBLE
-        DirectoryLoadingTask().execute(directory)
-        history.push(directory)
+        when (directory) {
+            is IFilePrimitive -> {
+                if (directory is Group)
+                    currentGroup = directory
+                else
+                    history.push(directory)
+                FilesLoadingTask().execute(directory)
+            }
+            null -> {
+                history.clear()
+                currentGroup = null
+                FilesLoadingTask().execute(null)
+            }
+        }
         (activity as AppCompatActivity).supportActionBar?.title = getTitle()
     }
 
     override fun onBackPressed(): Boolean {
-        if (history.size > 1) {
-            history.pop()
-            navigate(history.pop()) // item will be pushed again by navigate() //FIXME maybe not that nice to reset every scroll index on going back
-            return true
+        return when {
+            history.isNotEmpty() -> {
+                history.pop()
+                navigate(if (history.isEmpty()) currentGroup else history.pop()) // item will be pushed again by navigate() //FIXME maybe not that nice to reset every scroll index on going back
+                true
+            }
+            currentGroup != null -> {
+                navigate(null) // show groups
+                true
+            }
+            else -> false
         }
-        return false
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putSerializable(SAVE_HISTORY, history)
+        if (currentGroup != null)
+            outState.putSerializable(SAVE_GROUP, currentGroup)
     }
 
     override fun getTitle(): String {
@@ -111,29 +140,47 @@ class FileStorageFragment: FeatureFragment(AppFeature.FEATURE_FILE_STORAGE), IBa
         else getString(R.string.file_storage)
     }
 
-    private inner class DirectoryLoadingTask: AsyncTask<IFilePrimitive, Void, List<OnlineFile>?>() {
+    private inner class FilesLoadingTask: AsyncTask<Any?, Void, FilesLoadingTask.Result>() {
 
-        override fun doInBackground(vararg params: IFilePrimitive): List<OnlineFile>? {
+        override fun doInBackground(vararg params: Any?): Result {
             return try {
-                params[0].getFileStorageFiles(true) // don't want to cache file request here
-                    .sortedByDescending { it.type }
+                when (val param = params[0]) {
+                    is IFilePrimitive -> {
+                        Result(param.getFileStorageFiles(true).sortedByDescending { it.type }, null)
+                    }
+                    else -> Result(AuthStore.appUser.getContext().getGroups().map { try {
+                        Pair(it, it.getFileStorageState().second)
+                    } catch(e: Exception) { Pair(it, Quota(-1, -1, -1, -1, -1, -1)) } }, null)
+                }
             } catch (e: Exception) {
-                null
+                Result(null, e)
             }
         }
 
-        override fun onPostExecute(result: List<OnlineFile>?) {
+        override fun onPostExecute(result: Result) {
             progress_file_storage?.visibility = ProgressBar.INVISIBLE
             file_storage_swipe_refresh?.isRefreshing = false
             if (context != null) {
-                if (result != null) {
-                    file_list?.adapter = FileStorageAdapter(context!!, result)
-                    file_empty.isVisible = result.isEmpty()
-                } else {
-                    Toast.makeText(context, getString(R.string.request_failed_other).format("No details"), Toast.LENGTH_LONG).show()
+                if (result.result != null) {
+                    if (result.result is List<*> && result.result.isNotEmpty()) {
+                        if (result.result.first() is OnlineFile) {
+                            @Suppress("UNCHECKED_CAST")
+                            file_list?.adapter = FileStorageFilesAdapter(context!!, result.result as List<OnlineFile>)
+                            file_empty.isVisible = result.result.isEmpty()
+                        } else if (result.result.first() is Pair<*, *>) {
+                            @Suppress("UNCHECKED_CAST")
+                            file_list?.adapter = FileStorageAdapter(context!!, (result.result as List<Pair<Group, Quota>>).toMap())
+                            file_empty.isVisible = result.result.isEmpty()
+                        }
+                    }
+
+                } else if (result.exception != null) {
+                    Toast.makeText(context, getString(R.string.request_failed_other).format(result.exception.message), Toast.LENGTH_LONG).show()
                 }
             }
         }
+
+        private inner class Result(val result: Any?, val exception: Throwable?)
 
     }
 
