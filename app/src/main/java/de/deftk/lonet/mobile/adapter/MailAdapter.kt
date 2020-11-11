3384ccf6c45917820756f34af0d2e182b4b7cf2b
deftkHD <32391033+deftkHD@users.noreply.github.com>
2020-11-11 16:55:16 +0100

    added swipe delete gesture for emails

--- app/src/main/java/de/deftk/lonet/mobile/adapter/MailAdapter.kt
+++ app/src/main/java/de/deftk/lonet/mobile/adapter/MailAdapter.kt
@@ -7,16 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import com.daimajia.swipe.SwipeLayout
 import de.deftk.lonet.api.model.feature.mailbox.Email
 import de.deftk.lonet.mobile.AuthStore
 import de.deftk.lonet.mobile.R
+import de.deftk.lonet.mobile.utils.SwipeAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.DateFormat
 
 class MailAdapter(context: Context, elements: List<Email>): ArrayAdapter<Email>(context, 0, elements) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val listItemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_mail, parent, false)
+        val listItemView = (convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_mail, parent, false)) as SwipeLayout
         val item = getItem(position) ?: return listItemView
+
+        listItemView.addSwipeListener(object : SwipeAdapter() {
+            override fun onOpen(layout: SwipeLayout) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    item.delete()
+                    withContext(Dispatchers.Main) {
+                        remove(item)
+                        notifyDataSetChanged()
+                    }
+                }
+            }
+        })
 
         val subjectView = listItemView.findViewById<TextView>(R.id.mail_subject)
         subjectView.text = item.subject
