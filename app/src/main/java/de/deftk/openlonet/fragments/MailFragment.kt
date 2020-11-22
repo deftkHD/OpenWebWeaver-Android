4b6fe93eb2eac769cfcf372a41c6476b7b4c7868
deftkHD <32391033+deftkHD@users.noreply.github.com>
2020-11-22 12:41:55 +0100

    choose mail folders from spinner inside toolbox

--- app/src/main/java/de/deftk/openlonet/fragments/MailFragment.kt
+++ app/src/main/java/de/deftk/openlonet/fragments/MailFragment.kt
@@ -15,7 +15,9 @@ import de.deftk.openlonet.AuthStore
 import de.deftk.openlonet.R
 import de.deftk.openlonet.abstract.FeatureFragment
 import de.deftk.openlonet.abstract.IBackHandler
-import de.deftk.openlonet.activities.feature.mail.MailsActivity
+import de.deftk.openlonet.activities.feature.mail.ReadMailActivity
+import de.deftk.openlonet.activities.feature.mail.WriteMailActivity
+import de.deftk.openlonet.adapter.MailAdapter
 import de.deftk.openlonet.adapter.MailFolderAdapter
 import de.deftk.openlonet.feature.AppFeature
 import kotlinx.android.synthetic.main.fragment_mail.*
@@ -23,58 +25,62 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.Serializable
 
 class MailFragment: FeatureFragment(AppFeature.FEATURE_MAIL), IBackHandler {
 
     //TODO context menu
 
+    companion object {
+        const val REQUEST_CODE_WRITE_MAIL = 1
+    }
+
+    private lateinit var toolbarSpinner: Spinner
+    private var currentFolder: EmailFolder? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         setHasOptionsMenu(true)
+
+        toolbarSpinner = requireActivity().findViewById(R.id.toolbar_spinner)
+        toolbarSpinner.visibility = View.VISIBLE
+        toolbarSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val folder = toolbarSpinner.getItemAtPosition(position) as EmailFolder
+                currentFolder = folder
+                reloadEmails()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
 
         val view = inflater.inflate(R.layout.fragment_mail, container, false)
         val list = view.findViewById<ListView>(R.id.mail_list)
         val swipeRefresh = view.findViewById<SwipeRefreshLayout>(R.id.mail_swipe_refresh)
         swipeRefresh.setOnRefreshListener {
-            reloadEmailFolders()
+            reloadEmails()
         }
         list.setOnItemClickListener { _, _, position, _ ->
-            val item = list.getItemAtPosition(position)
-            if (item is EmailFolder) {
-                val intent = Intent(requireContext(), MailsActivity::class.java)
-                intent.putExtra(MailsActivity.EXTRA_FOLDER, item)
-                startActivity(intent)
-            }
+            val intent = Intent(requireContext(), ReadMailActivity::class.java)
+            intent.putExtra(ReadMailActivity.EXTRA_MAIL, mail_list.getItemAtPosition(position) as Serializable)
+            startActivity(intent)
         }
-        reloadEmailFolders()
         if (AuthStore.appUser.effectiveRights.contains(Permission.MAILBOX_ADMIN)) {
-            val fab = view.findViewById<FloatingActionButton>(R.id.fab_mail_folder_add)
+            val fab = view.findViewById<FloatingActionButton>(R.id.fab_mail_add)
             fab.visibility = View.VISIBLE
             fab.setOnClickListener {
-                val builder = AlertDialog.Builder(requireContext())
-                builder.setTitle(R.string.create_new_folder)
-
-                val input = EditText(requireContext())
-                input.hint = getString(R.string.name)
-                input.inputType = InputType.TYPE_CLASS_TEXT
-                builder.setView(input)
-
-                builder.setPositiveButton(R.string.confirm) { _, _ ->
-                    CoroutineScope(Dispatchers.IO).launch {
-                        createNewFolder(input.text.toString())
-                    }
-                }
-                builder.setNegativeButton(R.string.cancel) { dialog, _ ->
-                    dialog.cancel()
-                }
-
-                builder.show()
+                val intent = Intent(requireContext(), WriteMailActivity::class.java)
+                startActivityForResult(intent, REQUEST_CODE_WRITE_MAIL)
             }
         }
+
+        reloadEmailFolders()
         return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
+
+        // search menu
         inflater.inflate(R.menu.list_filter_menu, menu)
         val searchItem = menu.findItem(R.id.filter_item_search)
         val searchView = searchItem.actionView as SearchView
@@ -89,11 +95,44 @@ class MailFragment: FeatureFragment(AppFeature.FEATURE_MAIL), IBackHandler {
                 return false
             }
         })
+
+        // utility menu
+        inflater.inflate(R.menu.mail_list_menu, menu)
+
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.mail_list_menu_add_folder) {
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle(R.string.create_new_folder)
+
+            val input = EditText(requireContext())
+            input.hint = getString(R.string.name)
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            builder.setView(input)
+
+            builder.setPositiveButton(R.string.confirm) { _, _ ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    createNewFolder(input.text.toString())
+                }
+            }
+            builder.setNegativeButton(R.string.cancel) { dialog, _ ->
+                dialog.cancel()
+            }
+
+            builder.show()
+            return true
+        } else return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed(): Boolean {
         return false
+    }
+
+    override fun onDestroy() {
+        toolbarSpinner.visibility = View.GONE
+        super.onDestroy()
     }
 
     private suspend fun createNewFolder(name: String) {
@@ -116,7 +155,7 @@ class MailFragment: FeatureFragment(AppFeature.FEATURE_MAIL), IBackHandler {
     }
 
     private fun reloadEmailFolders() {
-        mail_list?.adapter = null
+        toolbarSpinner.adapter = null
         CoroutineScope(Dispatchers.IO).launch {
             loadEmailFolders()
         }
@@ -126,8 +165,32 @@ class MailFragment: FeatureFragment(AppFeature.FEATURE_MAIL), IBackHandler {
         try {
             val folders = AuthStore.appUser.getEmailFolders()
             withContext(Dispatchers.Main) {
-                mail_list?.adapter = MailFolderAdapter(requireContext(), folders)
-                mail_empty?.isVisible = folders.isEmpty()
+                toolbarSpinner.adapter = MailFolderAdapter(requireContext(), folders)
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    context,
+                    getString(R.string.request_failed_other).format(e.message ?: e),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun reloadEmails() {
+        mail_list?.adapter = null
+        CoroutineScope(Dispatchers.IO).launch {
+            loadEmails()
+        }
+    }
+
+    private suspend fun loadEmails() {
+        try {
+            val emails = currentFolder?.getEmails() ?: emptyList()
+            withContext(Dispatchers.Main) {
+                mail_list.adapter = MailAdapter(requireContext(), emails)
+                mail_empty?.isVisible = emails.isEmpty()
                 progress_mail?.visibility = ProgressBar.GONE
                 mail_swipe_refresh?.isRefreshing = false
             }
@@ -144,8 +207,6 @@ class MailFragment: FeatureFragment(AppFeature.FEATURE_MAIL), IBackHandler {
         }
     }
 
-    override fun getTitle(): String {
-        return getString(R.string.mail)
-    }
+    override fun getTitle() = ""
 
 }
