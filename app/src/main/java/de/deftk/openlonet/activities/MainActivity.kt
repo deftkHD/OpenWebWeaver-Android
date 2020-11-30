665afa23806a5c05b41d6ba2dfdddd68360c5b43
deftkHD <32391033+deftkHD@users.noreply.github.com>
2020-11-30 21:31:56 +0100

    use scoped storage

--- app/src/main/java/de/deftk/openlonet/activities/MainActivity.kt
+++ app/src/main/java/de/deftk/openlonet/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package de.deftk.openlonet.activities
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import de.deftk.openlonet.AuthStore
 import de.deftk.openlonet.BuildConfig
 import de.deftk.openlonet.R
@@ -37,46 +31,16 @@ class MainActivity : AppCompatActivity() {
 
         // check if app has write & read permission
         Log.i(LOG_TAG, "Checking permissions")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            Log.i(LOG_TAG, "Requesting permissions")
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                AlertDialog.Builder(this)
-                    .setMessage(R.string.files_permission_description)
-                    .setPositiveButton(R.string.confirm) { _, _ ->
-                        requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-                    }
-                    .show()
-            } else {
-                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-            }
-        } else {
-            CoroutineScope(Dispatchers.IO).launch {
-                if (AuthStore.performLogin(this@MainActivity)) {
-                    val intent = Intent(this@MainActivity, StartActivity::class.java)
-                    withContext(Dispatchers.Main) {
-                        startActivity(intent)
-                    }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            if (AuthStore.performLogin(this@MainActivity)) {
+                val intent = Intent(this@MainActivity, StartActivity::class.java)
+                withContext(Dispatchers.Main) {
+                    startActivity(intent)
                 }
             }
         }
         Log.i(LOG_TAG, "Created MainActivity")
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        Log.i(LOG_TAG, "Permission result")
-        when (requestCode) {
-            0 -> {
-                if (grantResults.getOrNull(0) != PackageManager.PERMISSION_GRANTED) {
-                    Log.i(LOG_TAG, "Permission not granted")
-                    finish()
-                } else {
-                    Log.i(LOG_TAG, "Permission granted")
-                    CoroutineScope(Dispatchers.IO).launch {
-                        AuthStore.performLogin(this@MainActivity)
-                    }
-                }
-            }
-        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/de/deftk/openlonet/activities/feature/FilesActivity.kt
+++ app/src/main/java/de/deftk/openlonet/activities/feature/FilesActivity.kt
@@ -1,21 +1,19 @@
 package de.deftk.openlonet.activities.feature
 
-import android.app.DownloadManager
 import android.content.*
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
+import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import androidx.core.net.toFile
 import androidx.core.view.isVisible
 import androidx.preference.PreferenceManager
 import de.deftk.lonet.api.model.abstract.AbstractOperator
 import de.deftk.lonet.api.model.feature.abstract.IFilePrimitive
 import de.deftk.lonet.api.model.feature.files.OnlineFile
-import de.deftk.openlonet.BuildConfig
 import de.deftk.openlonet.R
 import de.deftk.openlonet.adapter.FileStorageFilesAdapter
 import de.deftk.openlonet.utils.FileUtil
@@ -24,32 +22,27 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.net.URL
 
 class FilesActivity : AppCompatActivity() {
 
     //TODO show preview image if possible
 
     companion object {
+        private const val FILE_PROVIDER_AUTHORITY = "de.deftk.openlonet.fileprovider"
+
         const val EXTRA_FOLDER = "de.deftk.openlonet.files.extra_folder"
     }
 
     private lateinit var fileStorage: IFilePrimitive
 
+    private val requestedActivities = mutableMapOf<Int, RequestableAction>()
     private val preferences by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
-
-    private val onCompleteReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (intent.action == DownloadManager.ACTION_DOWNLOAD_COMPLETE) {
-                val downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0)
-                openDownload(context, downloadId)
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_files)
-        registerReceiver(onCompleteReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         val extraFolder = intent.getSerializableExtra(EXTRA_FOLDER) as? IFilePrimitive?
         if (extraFolder != null) {
@@ -75,9 +68,8 @@ class FilesActivity : AppCompatActivity() {
             val item = file_list.getItemAtPosition(position) as IFilePrimitive
             if (item is OnlineFile) {
                 if (item.type == OnlineFile.FileType.FILE) {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        downloadFile(item)
-                    }
+                    if (item.effectiveRead == true)
+                        openFile(item)
                 } else if (item.type == OnlineFile.FileType.FOLDER) {
                     val i = Intent(this, FilesActivity::class.java)
                     i.putExtra(EXTRA_FOLDER, item)
@@ -87,6 +79,7 @@ class FilesActivity : AppCompatActivity() {
             }
         }
 
+        registerForContextMenu(file_list)
         reloadFiles()
     }
 
@@ -109,15 +102,106 @@ class FilesActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if (menuInfo is AdapterView.AdapterContextMenuInfo) {
+            val file = file_list?.adapter?.getItem(menuInfo.position) as OnlineFile
+            if (file.effectiveRead == true) {
+                menuInflater.inflate(R.menu.filestorage_read_list_menu, menu)
+            }
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.filestorage_action_download -> {
+                val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+                val file = file_list?.adapter?.getItem(info.position) as OnlineFile
+                val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+                intent.type = FileUtil.getMimeType(file.name)
+                intent.putExtra(Intent.EXTRA_TITLE, file.name)
+                startActivityForResult(intent, getRequestCode(file, FileAction.DOWNLOAD_SAVE))
+                true
+            }
+            R.id.filestorage_action_open -> {
+                val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+                val file = file_list?.adapter?.getItem(info.position) as OnlineFile
+                openFile(file)
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val action = requestedActivities[requestCode]
+        if (action != null && resultCode == RESULT_OK && data?.data != null) {
+            when (action.action) {
+                FileAction.DOWNLOAD_SAVE -> {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        withContext(Dispatchers.IO) {
+                            check(action.target is OnlineFile) { "Invalid target; must be of type OnlineFile" }
+                            val outputStream = contentResolver.openOutputStream(data.data!!, "w") ?: return@withContext
+                            val inputStream = URL(action.target.getTempDownloadUrl().downloadUrl).openStream()
+                            val buffer = ByteArray(2048)
+                            while (true) {
+                                val read = inputStream.read(buffer)
+                                if (read <= 0)
+                                    break
+                                outputStream.write(buffer, 0, read)
+                            }
+                            outputStream.close()
+                            inputStream.close()
+                        }
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@FilesActivity, R.string.download_finished, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     // back button in toolbar functionality
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
-    override fun onDestroy() {
-        unregisterReceiver(onCompleteReceiver)
-        super.onDestroy()
+    private fun openFile(file: OnlineFile) {
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                val mime = FileUtil.getMimeType(file.name)
+                val download = file.getTempDownloadUrl().downloadUrl
+                val inputStream = URL(download).openStream() ?: return@withContext
+                val tempDir = File(cacheDir, "filestorage")
+                if (!tempDir.isDirectory)
+                    tempDir.mkdir()
+                val tempFile = File(tempDir, file.name)
+                inputStream.copyTo(tempFile.outputStream())
+                inputStream.close()
+                val fileUri = FileProvider.getUriForFile(this@FilesActivity, FILE_PROVIDER_AUTHORITY, tempFile)
+
+                val sendIntent = Intent(Intent.ACTION_SEND)
+                sendIntent.type = mime
+                sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                sendIntent.putExtra(Intent.EXTRA_STREAM, fileUri)
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, normalizeFileName(file.name))
+                val viewIntent = Intent(Intent.ACTION_VIEW)
+                viewIntent.setDataAndType(fileUri, mime)
+                viewIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivity(Intent.createChooser(sendIntent, file.name).apply { putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(viewIntent)) })
+            }
+        }
+    }
+
+    private fun getRequestCode(onlineFile: OnlineFile, action: FileAction): Int {
+        val id = requestedActivities.size
+        requestedActivities[id] = RequestableAction(onlineFile, action)
+        return id
     }
 
     private fun reloadFiles() {
@@ -150,77 +234,6 @@ class FilesActivity : AppCompatActivity() {
         }
     }
 
-    //TODO scoped storage
-    private suspend fun downloadFile(file: OnlineFile) {
-        try {
-            val downloadUrl = file.getTempDownloadUrl().downloadUrl
-            val request = DownloadManager.Request(Uri.parse(downloadUrl))
-            val fileName = file.name.replace("[^0-9a-zA-Z_.]".toRegex(), "")
-            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
-            request.setTitle("Download")
-            request.setDescription(file.name)
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-            request.setDestinationInExternalPublicDir(
-                Environment.DIRECTORY_DOWNLOADS,
-                "temp/$fileName"
-            )
-            request.setMimeType(FileUtil.getMimeType(file.name))
-
-            val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            downloadManager.enqueue(request)
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(
-                    this@FilesActivity,
-                    getString(R.string.request_failed_other).format(e.message ?: e),
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-    }
-
-    private fun openDownload(context: Context, downloadId: Long) {
-        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        val query = DownloadManager.Query()
-        query.setFilterById(downloadId)
-        val cursor = downloadManager.query(query)
-        if (cursor.moveToFirst()) {
-            val downloadStatus = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-            val downloadLocalUri =
-                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
-            val downloadMimeType =
-                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE))
-            if ((downloadStatus == DownloadManager.STATUS_SUCCESSFUL) && downloadLocalUri != null) {
-                openDownload(context, Uri.parse(downloadLocalUri), downloadMimeType)
-            }
-        }
-        cursor.close()
-    }
-
-    private fun openDownload(context: Context, fileUri: Uri?, mimeType: String?) {
-        if (fileUri != null) {
-            val sharedUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", fileUri.toFile())
-
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = mimeType
-            intent.clipData = ClipData.newRawUri(sharedUri.lastPathSegment, sharedUri)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            intent.putExtra(Intent.EXTRA_STREAM, sharedUri)
-            intent.putExtra(Intent.EXTRA_SUBJECT, normalizeFileName(sharedUri.lastPathSegment ?: "imported"))
-            try {
-                context.startActivity(Intent.createChooser(intent, sharedUri.lastPathSegment))
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(
-                    context,
-                    String.format(context.getText(R.string.download_open_failed).toString(), e.message ?: e),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
     private fun normalizeFileName(name: String): String {
         return if (preferences.getBoolean("file_storage_correct_file_names", false)) {
             if (name.contains('.')) {
@@ -231,6 +244,12 @@ class FilesActivity : AppCompatActivity() {
         } else {
             name
         }
+    }
+
+    private data class RequestableAction(val target: Any, val action: FileAction)
+
+    private enum class FileAction {
+        DOWNLOAD_SAVE
     }
 
 }
