e71b9107ca2e144aa58a56e4dbcd898a85baf96e
deftkHD <32391033+deftkHD@users.noreply.github.com>
2021-01-24 21:18:05 +0100

    added progress indicators for overview

--- app/src/main/java/de/deftk/openlonet/feature/overview/FileStorageOverview.kt
+++ app/src/main/java/de/deftk/openlonet/feature/overview/FileStorageOverview.kt
@@ -5,14 +5,17 @@ import android.text.format.Formatter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import de.deftk.lonet.api.model.feature.Quota
 import de.deftk.openlonet.R
 
-class FileStorageOverview(val quota: Quota): AbstractOverviewElement(object : AbstractOverviewAdapter() {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup, context: Context): View {
-        val itemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_overview_file_storage, parent, false)
-        itemView.findViewById<TextView>(R.id.overview_file_quota).text = String.format(context.getString(R.string.quota), Formatter.formatFileSize(context, quota.free), Formatter.formatFileSize(context, quota.limit))
-        return itemView
-    }
-})
+class FileStorageOverview(val quota: Quota) : AbstractOverviewElement(object : AbstractOverviewAdapter() {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup, context: Context): View {
+            val itemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_overview_file_storage, parent, false)
+            itemView.findViewById<TextView>(R.id.overview_file_quota).text = String.format(context.getString(R.string.quota), Formatter.formatFileSize(context, quota.free), Formatter.formatFileSize(context, quota.limit))
+            itemView.findViewById<ProgressBar>(R.id.progress_personal_file_storage).progress = ((quota.usage.toFloat() / quota.limit) * 100).toInt()
+            return itemView
+        }
+    })
--- app/src/main/java/de/deftk/openlonet/feature/overview/TasksOverview.kt
+++ app/src/main/java/de/deftk/openlonet/feature/overview/TasksOverview.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import de.deftk.openlonet.R
 
-class TasksOverview(val tasksDone: Int, val tasksAll: Int): AbstractOverviewElement(object : AbstractOverviewAdapter() {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup, context: Context): View {
-        val itemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_overview_tasks, parent, false)
-        itemView.findViewById<TextView>(R.id.overview_tasks_done).text = String.format(context.getString(R.string.tasks_done), tasksDone, tasksAll)
-        return itemView
-    }
-})
+class TasksOverview(val tasksDone: Int, val tasksAll: Int) : AbstractOverviewElement(object : AbstractOverviewAdapter() {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup, context: Context): View {
+            val itemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item_overview_tasks, parent, false)
+            itemView.findViewById<TextView>(R.id.overview_tasks_done).text = String.format(context.getString(R.string.tasks_done), tasksDone, tasksAll)
+            itemView.findViewById<ProgressBar>(R.id.progress_tasks_done).progress = ((tasksDone.toFloat() / tasksAll) * 100).toInt()
+            return itemView
+        }
+
+    })
