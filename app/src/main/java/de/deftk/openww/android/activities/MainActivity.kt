fc6fc27c6caf47af3bd906d37ef10ce38d55a190
deftkHD <32391033+deftkHD@users.noreply.github.com>
2021-06-26 14:19:14 +0200

    version 0.1.0

--- app/src/main/java/de/deftk/openww/android/activities/MainActivity.kt
+++ app/src/main/java/de/deftk/openww/android/activities/MainActivity.kt
@@ -3,7 +3,6 @@ package de.deftk.openww.android.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
