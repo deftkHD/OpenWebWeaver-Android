d1b98cf8448ded3f11f0dfeab28df0aa295fb447
deftkHD <32391033+deftkHD@users.noreply.github.com>
2021-06-30 15:06:19 +0200

    revert! this wont compile

--- app/src/main/java/de/deftk/openww/android/OpenWebWeaver.kt
+++ app/src/main/java/de/deftk/openww/android/OpenWebWeaver.kt
@@ -2,19 +2,6 @@ package de.deftk.openww.android
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import de.deftk.openww.android.feature.contacts.sync.ContactsSyncAdapter
 
 @HiltAndroidApp
-class OpenWebWeaver : Application() {
-
-    companion object {
-        lateinit var contactsSyncAdapter: ContactsSyncAdapter
-            private set
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        contactsSyncAdapter = ContactsSyncAdapter(applicationContext)
-    }
-
-}
+class OpenWebWeaver : Application()
