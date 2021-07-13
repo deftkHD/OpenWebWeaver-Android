41d8c482dcf31d6e4f0be8742ff0b54a8368fe00
deftkHD <32391033+deftkHD@users.noreply.github.com>
2021-07-13 21:28:19 +0200

    fixed messages appearing twice inside messenger

--- app/src/main/java/de/deftk/openww/android/repository/MessengerRepository.kt
+++ app/src/main/java/de/deftk/openww/android/repository/MessengerRepository.kt
@@ -4,6 +4,8 @@ import de.deftk.openww.android.room.QuickMessageDao
 import de.deftk.openww.android.room.RoomQuickMessage
 import de.deftk.openww.api.implementation.ApiContext
 import de.deftk.openww.api.model.feature.filestorage.session.ISessionFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MessengerRepository @Inject constructor(private val quickMessageDao: QuickMessageDao) : AbstractRepository() {
@@ -24,10 +26,20 @@ class MessengerRepository @Inject constructor(private val quickMessageDao: Quick
         val onlineMessages = apiContext.user.getHistory(exportSessionFile = true, context = apiContext.user.getRequestContext(apiContext))
             .filter { it.to.login == with || it.from.login == with }
             .toMutableList()
-        val savedMessages = quickMessageDao.getHistoryWith(with)
-        val newMessages = onlineMessages.filter { receivedMessage -> savedMessages.none { it.id == receivedMessage.id } }
-        quickMessageDao.insertMessages(newMessages.map { RoomQuickMessage.from(it) })
-        onlineMessages.addAll(savedMessages.map { it.toQuickMessage() })
+        withContext(Dispatchers.Unconfined) { // don't execute at network thread
+            val savedMessages = quickMessageDao.getHistoryWith(with)
+            val newMessages = onlineMessages.filter { receivedMessage -> savedMessages.none { it.id == receivedMessage.id } }
+            quickMessageDao.insertMessages(newMessages.map { RoomQuickMessage.from(it) })
+            if (savedMessages.size != onlineMessages.size) {
+                // merge
+                savedMessages.forEach { savedMessage ->
+                    if (!onlineMessages.any { it.id == savedMessage.id }) {
+                        onlineMessages.add(savedMessage.toQuickMessage())
+                    }
+                }
+            }
+        }
+
         onlineMessages.sortedBy { it.date.time }
     }
 
