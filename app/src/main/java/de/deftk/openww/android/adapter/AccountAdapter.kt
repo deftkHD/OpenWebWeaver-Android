4181d5d5a77170e04c513cbd5f89a83517a8bab8
deftkHD <32391033+deftkHD@users.noreply.github.com>
2021-07-31 22:03:13 +0200

    highlight background of current account in account chooser

--- app/src/main/java/de/deftk/openww/android/adapter/AccountAdapter.kt
+++ app/src/main/java/de/deftk/openww/android/adapter/AccountAdapter.kt
@@ -19,9 +19,13 @@ class AccountAdapter(context: Context, elements: List<Account>, private val user
         val split = item.name.split("@")
         val titleTextView = listItemView.findViewById<TextView>(R.id.account_name)
         titleTextView.text = split[0]
-        if (item.name == userViewModel.apiContext.value?.user?.login)
+        if (item.name == userViewModel.apiContext.value?.user?.login) {
             titleTextView.setTypeface(null, Typeface.BOLD)
-        else titleTextView.setTypeface(null, Typeface.NORMAL)
+            listItemView.setBackgroundResource(R.drawable.selected_account_item)
+        } else {
+            titleTextView.setTypeface(null, Typeface.NORMAL)
+            listItemView.setBackgroundResource(android.R.color.transparent)
+        }
         listItemView.findViewById<TextView>(R.id.account_provider).text = split[1]
         return listItemView
     }
